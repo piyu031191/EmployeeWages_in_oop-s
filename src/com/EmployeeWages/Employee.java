49789e956cef33d84e60a4f8f6525ec2aea5658d
piyu031191 <priyankasambatkar0864@gmail.com>
2021-11-14 18:22:40 +0530

    Employee wages with OOP's concept

---
+++ src/com/EmployeeWages/Employee.java
@@ -0,0 +1,53 @@
+package com.EmployeeWages;
+
+import java.util.Scanner;
+
+public class Employee {
+public void computeEmployeeWage(String name, int no_of_days, int max_hours_in_month, int wages_per_hour) {	
+		
+		Scanner input = new Scanner(System.in);
+		
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
+		final int PART_TIME_HOURS = 4;
+		final int FULL_TIME_HOURS = 8;
+		
+		int wage = 0;    
+		int days = 0;    
+		int hours = 0;   
+		
+		while (days < no_of_days && hours < max_hours_in_month) {
+			int attendance = (int)Math.floor( (Math.random() * 10) % 3);
+			
+			switch (attendance) {
+case 0:
+				break;
+				
+			case IS_PART_TIME: 
+					if (hours + PART_TIME_HOURS > 100) {
+						break;
+					}
+					hours += PART_TIME_HOURS;
+					days++;
+					break;
+			case IS_FULL_TIME:
+					if (hours + FULL_TIME_HOURS > 100) {
+						break;
+					}
+					hours += FULL_TIME_HOURS;
+					days++;
+					break;				
+			}
+		}
+		
+		wage = hours * wages_per_hour;
+		
+		System.out.println(" ****** " + name + " ******");
+		System.out.println(" TOTAL WAGES= " + wage);
+		
+System.out.println(" TOTAL WORKING DAYS = " + days);
+		System.out.println(" TOTAL WORKING HOURS = " + hours);	
+	}
+
+
+}
---
+++ src/com/EmployeeWages/EmployeeM.java
@@ -0,0 +1,38 @@
+package com.EmployeeWages;
+
+import java.util.Scanner;
+
+public class EmployeeM {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String name;
+		
+		System.out.print(" ENTER THE NO OF COMPONY: ");
+		int n = input.nextInt();
+		
+		Employee obj = new Employee();
+		
+		for (int i = 0; i < n; i++) {
+			
+			input.nextLine(); // to read the extra \n before it
+			System.out.print("\n\n ENTER THE NAME OF COMPONY: ");
+			name = input.nextLine();
+			
+			System.out.print(" ENTER MAX WORKING DAYS IN ONE A MONTH: ");
+			
+int no_of_days = input.nextInt();
+			
+			System.out.print("  ENTER MAX WORKING HOURS IN ONE A MONTH: ");
+			int max_hours_in_month = input.nextInt();
+			
+			System.out.print("ENTER DAILY WAGES PER HOUR: ");
+			int wages_per_hour = input.nextInt();
+			
+			obj.computeEmployeeWage(name, no_of_days, max_hours_in_month, wages_per_hour);
+			name = "";
+		}
+		input.close();
+	}
+
+}
